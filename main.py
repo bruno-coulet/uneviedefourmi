5d1cf99eb44bde9cd335c100c20b0bcf14d59562
Bruno Coulet <bruno.coulet@laplateforme.io>
2025-09-23 16:20:59 +0200

    uv et debut de classe AntNest

--- main.py
+++ main.py
@@ -3,6 +3,49 @@ résolution des fourmilières.
 '''
 
 import networkx as nx
-G = nx.Graph()
+import matplotlib.pyplot as plt
 
+#  undirefted graph
+G = nx.Graph()
+#  directed graph : de A vers B versus de B vers A
+G = nx.DiGraph()
+
+# ajouter une arête qui va de A à B, cela créa uassi les sommet s'ils n'existent pas encore
+G.add_edge('A', 'B')
+
+
+class AntNest:
+
+
+    def __init__(self, name : str, ants : int, rooms : dict[int, int], tubes : list[tuple[str, str]]):
+        '''fonction d'initialisation d'une fourmilière avec :
+        - le nom de la fourmilière
+        - le nombre de fourmis
+        - les salles (dict: id -> capacité)
+        - les tunnels (list: tuple: origine -> destination)
+        '''
+        self.name=name
+        self.ants=ants
+        self.rooms=rooms
+        self.tubes=tubes
+
+    def __str__(self) -> str:
+        '''représentation textuelle de la fourmilière'''
+        return (
+            f"{self.name}\n"
+            f"- Fourmis : {self.ants}\n"
+            f"- Salles  : {len(self.rooms)} ({self.rooms})\n"
+            f"- Tunnels : {len(self.tubes)} {self.tubes}"
+        )
+    
+    def __repr__(self):
+        return f"AntNest(ants={self.ants}, rooms={self.rooms}, tubes={self.tubes})"
+
+
+
+f1 = AntNest("fourmilière 1", 2, {1: 1, 2: 1}, ['v-s1','v-s2', 's1-d', 's2-d'] )
+
+
+
+print(f1)  # Appelle automatiquement __str__
 
